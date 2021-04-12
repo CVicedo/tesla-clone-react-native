f74077d27a31a589ebf508220876aa5826a3a837
Carles Vicedo <carlesvicedo@gmail.com>
2021-04-12 11:31:25 +0200

    Add Tescla clone project

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarsList from './components/CarsList'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
